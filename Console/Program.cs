0c7a4382ef9b0acfddeb4f7a21f1b348b090d2b1
xd43vild69 <xd43vild69@gmail.com>
2020-10-19 13:34:37 -0500

    Add angular project.

--- Console/Program.cs
+++ Console/Program.cs
@@ -21,7 +21,7 @@ namespace Console
             //OperateEF();
 
             //ReadFile
-            //OperateIOMemorize();
+            OperateIOMemorize();
 
             //Async
             //await OperateAsyncMethod();
